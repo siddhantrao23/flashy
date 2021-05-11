68911e0e1c55f16b7bc760f3eebbc755ad5f8787
Siddhant Rao <sid@pop-os.localdomain>
2021-05-11 19:38:39 +0530

    add set and card views

--- cards/urls.py
+++ cards/urls.py
@@ -5,7 +5,6 @@ from . import views
 app_name = 'cards'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('<int:pk>/', views.SetView.as_view(), name='set'),
+    path('<int:set_id>/<int:card_id>/', views.CardView.as_view(), name='card'),
 ]
--- cards/views.py
+++ cards/views.py
@@ -1,9 +1,9 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.views.generic.base import View, HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 
-from .models import Set, Question
+from .models import Set, Card
 
 class IndexView(generic.ListView):
     template_name = 'cards/index.html'
@@ -11,15 +11,27 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         return Set.objects.order_by('-set_name')
-        # return Question.objects.order_by('-pub_date')[:5]
 
-class DetailView(generic.DetailView):
-    model = Question
-    template_name = 'cards/detail.html'
+class SetView(generic.DetailView):
+    model = Set
+    template_name = 'cards/set.html'
 
-class ResultsView(generic.DetailView):
-    model = Question
-    template_name = 'cards/results.html'
+class CardView(View):
+    template_name = 'cards/card.html'
+
+    def get(self, request, set_id, card_id):
+        selected_set = get_object_or_404(Set, pk=set_id)
+        try:
+            selected_card = selected_set.card_set.get(id=card_id)
+            print(selected_card)
+            context = { 'set': selected_set, 'card': selected_card }
+        except (KeyError, Card.DoesNotExist):
+            return render(request, 'cards/set.html', {
+                'set': selected_set,
+                'error_message': "card not in range.",
+            })
+        else:
+            return render(request, self.template_name, context)
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)

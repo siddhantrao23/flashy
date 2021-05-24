907a2ab6af6e01e75ada23ea87a699f8bd4ccd25
Siddhant Rao <sid@pop-os.localdomain>
2021-05-24 20:37:30 +0530

    change allowed hosts

--- flashy/settings.py
+++ flashy/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-_65k5q0^(rx1-k@h0@bsh@670a-!-9@q0zhht#s2806^k-8ly7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'flashy-cards-app.herokuapp.com']
 
 # Application definition
 
